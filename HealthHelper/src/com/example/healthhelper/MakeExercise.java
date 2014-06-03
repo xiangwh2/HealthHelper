221ea689d5909ae0ba867cddf94a74da8dd06c0c
MCSYSU <zhzey@mail2.sysu.edu.cn>
2014-06-03 15:08:46 +0800

    时间控件完成

--- HealthHelper/src/com/example/healthhelper/MakeExercise.java
+++ HealthHelper/src/com/example/healthhelper/MakeExercise.java
@@ -24,9 +24,24 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 public class MakeExercise extends Activity {
-	private EditText m_EditText;
+	private EditText m_EditTextDate;
+	private EditText m_EditTextTime;
 	private Spinner m_Spinner;
 	private RadioGroup m_rGroup;
 	private RadioButton m_rButton0,m_rButton1;
@@ -36,7 +51,21 @@ public class MakeExercise extends Activity {
 	private String eType, dest, eTime;	
 	private boolean start;
 	private Button button_start, button_show;
+	private EditText showDate = null;
+	private Button pickDate = null;
+	private EditText showTime = null;
+	private Button pickTime = null;
 	
+	private static final int SHOW_DATAPICK = 0; 
+    private static final int DATE_DIALOG_ID = 1;  
+    private static final int SHOW_TIMEPICK = 2;
+    private static final int TIME_DIALOG_ID = 3;
+    
+    private int mYear;  
+    private int mMonth;
+    private int mDay; 
+    private int mHour;
+    private int mMinute;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		eType = "跑步";
@@ -46,9 +75,21 @@ public class MakeExercise extends Activity {
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_make_exercise);
-					
+		 initializeViews();
+	        
+	        final Calendar c = Calendar.getInstance();
+	        mYear = c.get(Calendar.YEAR);  
+	        mMonth = c.get(Calendar.MONTH);  
+	        mDay = c.get(Calendar.DAY_OF_MONTH);
+	        
+	        mHour = c.get(Calendar.HOUR_OF_DAY);
+	        mMinute = c.get(Calendar.MINUTE);
+	        
+	        setDateTime(); 
+	        setTimeOfDay();		
 		appHealthHelper = (HealthHelper)getApplicationContext();
-		m_EditText = (EditText) findViewById(R.id.editText1);
+		m_EditTextDate = (EditText) findViewById(R.id.showdate);
+		m_EditTextTime = (EditText) findViewById(R.id.showtime);
 		m_Spinner = (Spinner) findViewById(R.id.spinner1);
 		m_rGroup = (RadioGroup) findViewById(R.id.radioGroup1);
 		m_rButton0 = (RadioButton) findViewById(R.id.radio0);
@@ -83,7 +124,7 @@ public class MakeExercise extends Activity {
 				if (button_start.getText().toString() == "开始") {				
 					button_start.setText("停止");	
 					start = true;
-					eTime = m_EditText.getText().toString();												
+					eTime = m_EditTextDate.getText().toString()+"   "+m_EditTextTime.getText().toString();												
 					Exercise oneExercise = new Exercise(eType, eTime, dest);
 					appHealthHelper.getExerManager().addOneExercise(oneExercise);
 					
@@ -141,7 +182,7 @@ public class MakeExercise extends Activity {
 		// TODO Auto-generated method stub
 		if(keyCode==KeyEvent.KEYCODE_BACK&&event.getRepeatCount()==0){
 			Intent intent = new Intent();
-			intent.setClass(MakeExercise.this, MainActivity.class);
+			intent.setClass(MakeExercise.this, MakeExercise.class);
 			startActivity(intent);
 			MakeExercise.this.finish();	
 			return true;
@@ -166,9 +207,152 @@ public class MakeExercise extends Activity {
                     StepService.class));
     }
     
-    private void bindStepService() {   
+    @SuppressWarnings("unused")
+	private void bindStepService() {   
         bindService(new Intent(MakeExercise.this, 
                 StepService.class), mConnection, Context.BIND_AUTO_CREATE + Context.BIND_DEBUG_UNBIND);
     }
+    private void initializeViews(){
+        showDate = (EditText) findViewById(R.id.showdate);  
+        pickDate = (Button) findViewById(R.id.pickdate); 
+        showTime = (EditText)findViewById(R.id.showtime);
+        pickTime = (Button)findViewById(R.id.picktime);
+        
+        pickDate.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+	           Message msg = new Message(); 
+	           if (pickDate.equals((Button) v)) {  
+	              msg.what = MakeExercise.SHOW_DATAPICK;  
+	           }  
+	           MakeExercise.this.dateandtimeHandler.sendMessage(msg); 
+			}
+		});
+        
+        pickTime.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+	           Message msg = new Message(); 
+	           if (pickTime.equals((Button) v)) {  
+	              msg.what = MakeExercise.SHOW_TIMEPICK;  
+	           }  
+	           MakeExercise.this.dateandtimeHandler.sendMessage(msg); 
+			}
+		});
+    }
+
+    /**
+     * 设置日期
+     */
+	private void setDateTime(){
+       final Calendar c = Calendar.getInstance();  
+       
+       mYear = c.get(Calendar.YEAR);  
+       mMonth = c.get(Calendar.MONTH);  
+       mDay = c.get(Calendar.DAY_OF_MONTH); 
+  
+       updateDateDisplay(); 
+	}
 	
+	/**
+	 * 更新日期显示
+	 */
+	private void updateDateDisplay(){
+       showDate.setText(new StringBuilder().append(mYear).append("-")
+    		   .append((mMonth + 1) < 10 ? "0" + (mMonth + 1) : (mMonth + 1)).append("-")
+               .append((mDay < 10) ? "0" + mDay : mDay)); 
+	}
+	
+    /** 
+     * 日期控件的事件 
+     */  
+    private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {  
+  
+       public void onDateSet(DatePicker view, int year, int monthOfYear,  
+              int dayOfMonth) {  
+           mYear = year;  
+           mMonth = monthOfYear;  
+           mDay = dayOfMonth;  
+
+           updateDateDisplay();
+       }  
+    }; 
+	
+	/**
+	 * 设置时间
+	 */
+	private void setTimeOfDay(){
+	   final Calendar c = Calendar.getInstance(); 
+       mHour = c.get(Calendar.HOUR_OF_DAY);
+       mMinute = c.get(Calendar.MINUTE);
+       updateTimeDisplay();
+	}
+	
+	/**
+	 * 更新时间显示
+	 */
+	private void updateTimeDisplay(){
+       showTime.setText(new StringBuilder().append(mHour).append(":")
+               .append((mMinute < 10) ? "0" + mMinute : mMinute)); 
+	}
+    
+    /**
+     * 时间控件事件
+     */
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+		
+		@Override
+		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+			mHour = hourOfDay;
+			mMinute = minute;
+			
+			updateTimeDisplay();
+		}
+	};
+    
+    @Override  
+    protected Dialog onCreateDialog(int id) {  
+       switch (id) {  
+       case DATE_DIALOG_ID:  
+           return new DatePickerDialog(this, mDateSetListener, mYear, mMonth,  
+                  mDay);
+       case TIME_DIALOG_ID:
+    	   return new TimePickerDialog(this, mTimeSetListener, mHour, mMinute, true);
+       }
+    	   
+       return null;  
+    }  
+  
+    @Override  
+    protected void onPrepareDialog(int id, Dialog dialog) {  
+       switch (id) {  
+       case DATE_DIALOG_ID:  
+           ((DatePickerDialog) dialog).updateDate(mYear, mMonth, mDay);  
+           break;
+       case TIME_DIALOG_ID:
+    	   ((TimePickerDialog) dialog).updateTime(mHour, mMinute);
+    	   break;
+       }
+    }  
+  
+    /** 
+     * 处理日期和时间控件的Handler 
+     */  
+    Handler dateandtimeHandler = new Handler() {
+  
+       @Override  
+       public void handleMessage(Message msg) {  
+           switch (msg.what) {  
+           case MakeExercise.SHOW_DATAPICK:  
+               showDialog(DATE_DIALOG_ID);  
+               break; 
+           case MakeExercise.SHOW_TIMEPICK:
+        	   showDialog(TIME_DIALOG_ID);
+        	   break;
+           }  
+       }  
+  
+    }; 
 }
